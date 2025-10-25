3eda0563113ae764199b64b9536a55ab3896b9b2
Ambreen Hanif <ambreen.hanif@hdr.mq.edu.au>
2025-10-26 08:29:10 +1100

    evaluation removed

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -78,12 +78,12 @@ document.addEventListener('DOMContentLoaded', function() {
                     <span class="nav-text">ML Algorithms</span>
                 </a></li>
 
-                <li><a href="${basePath}evaluation/" class="nav-item" data-page="evaluation">
+                <!--<li><a href="${basePath}evaluation/" class="nav-item" data-page="evaluation">
                     <svg class="nav-icon" viewBox="0 0 24 24">
                         <path d="M12,2C13.1,2 14,2.9 14,4C14,5.1 13.1,6 12,6C10.9,6 10,5.1 10,4C10,2.9 10.9,2 12,2M21,9V7L15,1H5C3.89,1 3,1.89 3,3V21A2,2 0 0,0 5,23H19A2,2 0 0,0 21,21V9M19,9H14V4H19V9Z"/>
                     </svg>
                     <span class="nav-text">Evaluation</span>
-                </a></li>
+                </a></li>-->
             </ul>
         </nav>
     `;
